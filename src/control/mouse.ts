8256211921b539ecaf319322fa2fffdbb9c23952
bray <braydendiluca@gmail.com>
2025-11-25 21:21:46 +1100

    Correct mouse positioning based on canvas position

--- src/control/mouse.ts
+++ src/control/mouse.ts
@@ -1,25 +1,46 @@
+import type {Position} from "../math/vector.ts";
+
 export class MouseControl {
-    #x = 0;
-    #y = 0;
+    #position: Position = { x: 0, y: 0 };
+    #gameCanvas: HTMLCanvasElement;
 
     constructor(onMouseMove: (x: number, y: number) => void, onMouseClick: (x: number, y: number) => void) {
+        const canvas = document.getElementById('mainCanvas')
+        if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
+            throw new Error('Unable to find canvas');
+        }
+        this.#gameCanvas = canvas;
         this.#registerMouseMoveListener(onMouseMove)
         this.#registerMouseClickListener(onMouseClick)
     }
 
     #registerMouseClickListener(onMouseClick: (x: number, y: number) => void) {
-        window.addEventListener('click', (event: MouseEvent) => {
-            // Placeholder for future click handling logic
-            onMouseClick(event.x, event.y);
+        const mouseControlThis = this;
+        this.#gameCanvas.addEventListener('click', function (event: MouseEvent){
+            mouseControlThis.#position = mouseControlThis.#calculateTrueMouseLocation(
+                this.getBoundingClientRect(),
+                { x: event.clientX, y: event.clientY }
+            );
+            onMouseClick(mouseControlThis.#position.x, mouseControlThis.#position.y);
         });
     }
 
 
     #registerMouseMoveListener(onMouseMove: (x: number, y: number) => void) {
-        window.addEventListener('mousemove', (event: MouseEvent) => {
-            this.#x = event.clientX;
-            this.#y = event.clientY;
-            onMouseMove(this.#x, this.#y);
+        const mouseControlThis = this;
+        this.#gameCanvas.addEventListener('mousemove', function (event: MouseEvent) {
+            mouseControlThis.#position = mouseControlThis.#calculateTrueMouseLocation(
+                this.getBoundingClientRect(),
+                { x: event.clientX, y: event.clientY }
+            );
+            onMouseMove(mouseControlThis.#position.x, mouseControlThis.#position.y);
         });
+    }
+
+    #calculateTrueMouseLocation(boundingClient: DOMRect, mousePosition: Position): Position {
+        return {
+            x: mousePosition.x - (boundingClient.left),
+            y: mousePosition.y - (boundingClient.top),
+        }
     }
 }
