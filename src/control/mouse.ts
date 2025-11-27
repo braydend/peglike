c1954cc284199b19b775ae797161f9224349f742
Bray <braydendiluca@gmail.com>
2025-11-28 09:01:28 +1100

    ✨ feat: Disable firing when game has finished

--- src/control/mouse.ts
+++ src/control/mouse.ts
@@ -1,17 +1,22 @@
 import type {Position} from "../math/vector.ts";
+import type {Game} from "../game/game.ts";
+import {Logger} from "../logger/logger.ts";
 
 export class MouseControl {
     #position: Position = { x: 0, y: 0 };
     #gameCanvas: HTMLCanvasElement;
+    #clickAbortController: AbortController;
 
-    constructor(onMouseMove: (x: number, y: number) => void, onMouseClick: (x: number, y: number) => void) {
-        const canvas = document.getElementById('mainCanvas')
-        if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
-            throw new Error('Unable to find canvas');
-        }
-        this.#gameCanvas = canvas;
+    constructor(game: Game, onMouseMove: (x: number, y: number) => void, onMouseClick: (x: number, y: number) => void) {
+        this.#gameCanvas = game.getRenderer().getCanvas();
+        this.#clickAbortController = new AbortController();
         this.#registerMouseMoveListener(onMouseMove)
         this.#registerMouseClickListener(onMouseClick)
+    }
+
+    cleanup(): void {
+        Logger.debug("cleaning up mouse control listeners");
+        this.#clickAbortController.abort();
     }
 
     #registerMouseClickListener(onMouseClick: (x: number, y: number) => void) {
@@ -22,7 +27,7 @@ export class MouseControl {
                 { x: event.clientX, y: event.clientY }
             );
             onMouseClick(mouseControlThis.#position.x, mouseControlThis.#position.y);
-        });
+        }, { signal: this.#clickAbortController.signal });
     }
 
 
