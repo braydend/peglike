6cb9bf23158628b45b710f59ef630ee5afebd23c
Bray <braydendiluca@gmail.com>
2025-11-28 09:02:05 +1100

    ✨ feat: Add chrome for progressing levels

--- src/game/chrome/chrome.ts
+++ src/game/chrome/chrome.ts
@@ -1,4 +1,11 @@
+import {ChromeEventService} from "../service/chromeEventService.ts";
+import {Logger} from "../../logger/logger.ts";
+
 export class Chrome {
+    constructor() {
+        this.#addChromeContainer();
+    }
+
     #getAppContainer(): HTMLDivElement {
         const appElement = document.getElementById('app');
         if (!appElement || !(appElement instanceof HTMLDivElement)){
@@ -21,20 +28,66 @@ export class Chrome {
         appElement.removeChild(canvasElement);
     }
 
-    renderGameOverScreen(): void{
+    #addChromeContainer():void {
+        const appElement = this.#getAppContainer();
+        const chromeContainer = document.createElement('div');
+        chromeContainer.id = 'chrome';
+
+        appElement.appendChild(chromeContainer);
+
+    }
+
+    #getChromeContainer(): HTMLDivElement {
+        const chromeContainer = document.getElementById('chrome');
+        if (!chromeContainer || !(chromeContainer instanceof HTMLDivElement)){
+            throw new Error('Chrome container element not found.');
+        }
+        return chromeContainer;
+    }
+
+    renderGameOverScreen(highestLevel: number): void{
+        Logger.debug('Rendering game over screen');
         const appElement = this.#getAppContainer()
         this.#removeGameCanvas();
         const gameOverElement = document.createElement('div');
         gameOverElement.id = 'gameOverScreen';
         const gameOverHeading = document.createElement('h1');
         gameOverHeading.innerText = 'Game Over';
+        const gameOverText = document.createElement('p');
+        gameOverText.innerText = `You reached Level ${highestLevel}.`;
         const restartButton = document.createElement('button');
         restartButton.innerText = 'Restart Game';
         restartButton.onclick = () => {
             window.location.reload();
         };
         gameOverElement.appendChild(gameOverHeading);
+        gameOverElement.appendChild(gameOverText);
         gameOverElement.appendChild(restartButton);
         appElement.appendChild(gameOverElement);
+    }
+
+    renderLevelUpScreen(completedLevel: number): void{
+        Logger.debug('Rendering Level Up screen');
+        const chromeContainer = this.#getChromeContainer()
+        const gameOverElement = document.createElement('div');
+        gameOverElement.id = 'levelUpScreen';
+        const gameOverHeading = document.createElement('h1');
+        gameOverHeading.innerText = `Level ${completedLevel} Complete!`;
+        const restartButton = document.createElement('button');
+        restartButton.innerText = 'Next Level';
+        restartButton.onclick = () => {
+            ChromeEventService.emitLevelStartEvent(completedLevel);
+            this.clear();
+        };
+        gameOverElement.appendChild(gameOverHeading);
+        gameOverElement.appendChild(restartButton);
+        chromeContainer.appendChild(gameOverElement);
+    }
+
+    clear(): void {
+        const chromeContainer = this.#getChromeContainer();
+        for (const child of chromeContainer.children) {
+            child.remove();
+        }
     }
 }
