6d9cef63686ed15e0d12a1b412e32ad78fa955fc
Bray <braydendiluca@gmail.com>
2025-11-28 09:00:00 +1100

    ✨ feat: Add ChromeEventService

---
+++ src/game/service/chromeEventService.ts
@@ -0,0 +1,16 @@
+import type {Game} from "../game.ts";
+import {GameOverEvent, LevelCompleteEvent, LevelStartEvent} from "../chrome/events.ts";
+
+export class ChromeEventService {
+    static emitGameOverEvent(game: Game): void {
+        window.dispatchEvent(new GameOverEvent(game.getLevel()))
+    }
+
+    static emitLevelCompleteEvent(game: Game): void {
+        window.dispatchEvent(new LevelCompleteEvent(game.getLevel()))
+    }
+
+    static emitLevelStartEvent(level: number): void {
+        window.dispatchEvent(new LevelStartEvent(level))
+    }
+}
