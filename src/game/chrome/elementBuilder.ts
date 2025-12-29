888cbd09b4396bc2cd6c54bc38b0a4c5d208ffe0
bray <braydendiluca@gmail.com>
2025-12-29 18:40:58 +1100

    Add element builder for buttons

--- src/game/chrome/elementBuilder.ts
+++ src/game/chrome/elementBuilder.ts
@@ -1,12 +1,19 @@
-type DivOptions = {
+type CommonOptions = {
   id: string;
   className: string;
   attributes: Record<string, string>;
   text: string;
 };
 
+type DivOptions = Partial<CommonOptions>;
+
+type ButtonOptions = Partial<CommonOptions & {
+    disabled: boolean;
+    onClick: (event: MouseEvent) => void;
+}>;
+
 export class ElementBuilder {
-    static createDiv(parent: HTMLElement, options?: Partial<DivOptions>) {
+    static createDiv(parent: HTMLElement, options?: DivOptions) {
         const div = document.createElement("div");
         div.id = options?.id ?? "";
         div.className = options?.className ?? "";
@@ -18,5 +25,21 @@ export class ElementBuilder {
         }
         parent.appendChild(div);
         return div;
+    }
+
+    static createButton(parent: HTMLElement, options?: ButtonOptions) {
+        const button = document.createElement("button");
+        button.id = options?.id ?? "";
+        button.className = options?.className ?? "";
+        button.innerText = options?.text ?? "";
+        button.onclick = options?.onClick ?? null;
+        button.disabled = options?.disabled ?? false;
+        if (options?.attributes) {
+            Object.entries(options.attributes).forEach(([key, value]) => {
+                button.setAttribute(key, value);
+            })
+        }
+        parent.appendChild(button);
+        return button;
     }
 }
